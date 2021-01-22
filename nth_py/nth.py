0db049f9159bac24ae1dd11c9f972d5b7ae006ff
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2021-01-21 23:07:35 -0500

    Refactor to follow best practices

--- nth_py/nth.py
+++ nth_py/nth.py
@@ -2,6 +2,7 @@
 from typing import Optional, List, TextIO, Iterable
 from sys import argv, stdin, exit, stdout
 from pathlib import Path
+import logging
 
 import click
 
@@ -14,36 +15,42 @@ RC_NO_ARGS = 1
 READ_BYTES = 'rb'
 NEW_LINE = b'\n'
 
-def gen_lines_end(lines: List[int], input: TextIO=stdin) -> Iterable[str]:
+
+def gen_lines_end(lines: List[int], content: TextIO=stdin) -> Iterable[str]:
     pass
 
-def gen_lines(lines: List[int],
-              content: TextIO=stdin) -> Iterable[str]:
-  lines = sorted(lines)
-  line: int = None
+
+def gen_lines(
+    lines: List[int],
+    content: TextIO=stdin
+) -> Iterable[str]:
+  line_nums = sorted(lines)
+  # line: int = None
 
   for nth, line in enumerate(content):
-    if nth == lines[LOWEST]:
+    if nth == line_nums[LOWEST]:
       yield line
-      del lines[LOWEST]
+      del line_nums[LOWEST]
 
-      if not lines:
+      if not line_nums:
         break
 
-  #if lines and line:
-    #yield line
+  # if line_nums and line:
+    # yield line
 
 
-def exclude_lines(lines: List[int],
-                  content: TextIO=stdin) -> Iterable[str]:
-  lines = set(lines)
+def exclude_lines(
+    lines: List[int],
+    content: TextIO=stdin
+) -> Iterable[str]:
+  line_nums = set(lines)
 
   for nth, line in enumerate(content):
-    if nth not in lines:
+    if nth not in line_nums:
       yield line
 
     else:
-      lines.remove(nth)
+      line_nums.remove(nth)
 
 
 @click.command(help=f"""Return the contents of stdin from the line numbers supplied as arguments.
@@ -55,20 +62,24 @@ Example:
   world
 """)
 @click.argument('lines', nargs=-1, type=click.INT)
-@click.option('-r', '--reverse', is_flag=True, help=f"Write every line, except the line numbers supplied as LINES, from stdin to stdout.")
-@click.option('-f', '--file', help=f"Read from file instead of stdin.", type=click.File('r', lazy=True), default=None)
-@click.option('-e', '--empty', is_flag=True, help=f"Do not filter out blank lines, or lines consisting only of whitespace, from output.", default=False)
+@click.option('-r', '--reverse', is_flag=True, 
+    help=f"Write every line, except the line numbers supplied as LINES, from stdin to stdout.")
+@click.option('-f', '--file', type=click.File('r', lazy=True), default=None,
+    help=f"Read from file instead of stdin.")
+@click.option('-e', '--empty', is_flag=True, default=False
+    help=f"Do not filter out blank lines, or lines consisting only of whitespace, from output.")
 def cmd(lines: List[str], reverse: bool, file: TextIO, empty: bool):
   if not lines and not reverse:
-    print(f"No line numbers specified as arguments to {NAME}.")
+    logging.error(f"No line numbers specified as arguments to {NAME}.")
     exit(RC_NO_ARGS)
 
-  input = file if file else stdin.fileno()
+  content = file if file else stdin.fileno()
+  line_gen_func = exclude_lines if reverse else gen_lines
 
-  with open(input, READ_BYTES) as input:
-    output = (exclude_lines if reverse else gen_lines)(lines, input)
+  with open(input, READ_BYTES) as content:
+    output_lines = line_gen_func(lines, content)
 
-    for line in output:
+    for line in output_lines:
       if line or empty:  # filter out empty lines
         stdout.buffer.write(line)
 
